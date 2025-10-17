2c8c2eb448737c1adccc601d5c9fcd80a1a01376
MuchaiG <dennisgachuru2@gmail.com>
2025-10-17 15:39:14 +0300

    Second commit: accounts app setup

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('farmer', 'Farmer'),
+        ('staff', 'Staff'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='farmer')
+    phone_number = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
+from rest_framework import generics, permissions
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
